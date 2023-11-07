862254fe68f986cbc9ebdae7b515faed81842356
George Stoychev <georgi.stoychev.-nd@disney.com>
2023-11-07 20:06:01 +0200

    Fix file importer

--- import-service/importFileParser.js
+++ import-service/importFileParser.js
@@ -5,83 +5,18 @@ const {
   S3Client,
 } = require("@aws-sdk/client-s3");
 const { SendMessageCommand, SQSClient } = require("@aws-sdk/client-sqs");
+const { Readable } = require("stream");
 const csv = require("csv-parser");
+
 const BUCKET = "s3-import-product-bucket";
 const SQS_QUEUE_URL = "catalogItemsQueue";
 
-const client = new S3Client({ region: "eu-central-1" });
+const s3client = new S3Client({ region: "eu-central-1" });
 const sqsClient = new SQSClient({});
-
-const parseCSV = async (body) => ({
-  each: (callback) =>
-    new Promise((resolve, reject) => {
-      body
-        .pipe(csv())
-        .on("data", (data) => callback(data))
-        .on("error", (e) => reject(e))
-        .on("end", () => resolve(true));
-    }),
-});
-const createCommand = ({ title, description, price, count }, message) => {
-  console.log("command", {
-    Title: {
-      DataType: "String",
-      StringValue: title,
-    },
-    Description: {
-      DataType: "String",
-      StringValue: description,
-    },
-    Price: {
-      DataType: "Number",
-      StringValue: price,
-    },
-    Count: {
-      DataType: "Number",
-      StringValue: count,
-    },
-  });
-  return new SendMessageCommand({
-    QueueUrl: SQS_QUEUE_URL,
-    MessageAttributes: {
-      Title: {
-        DataType: "String",
-        StringValue: title,
-      },
-      Description: {
-        DataType: "String",
-        StringValue: description,
-      },
-      Price: {
-        DataType: "Number",
-        StringValue: price,
-      },
-      Count: {
-        DataType: "Number",
-        StringValue: count,
-      },
-    },
-    MessageBody: message,
-  });
-};
-
-const sendMessage = async (product) => {
-  console.log(product, JSON.stringify(product));
-
-  sqsClient
-    .send(
-      createCommand(
-        { title: "Big", description: "shit", price: 2, count: 3 },
-        JSON.stringify(product)
-      )
-    )
-    .then("ok")
-    .catch((e) => console.log(e.message, e));
-};
 
 module.exports.importFileParser = async (event) => {
   const key = event.Records[0].s3.object.key;
-  const getObjectCmd = new GetObjectCommand({
+  const getCmd = new GetObjectCommand({
     Bucket: BUCKET,
     Key: key,
   });
@@ -92,12 +27,44 @@ module.exports.importFileParser = async (event) => {
   });
   const deleteCmd = new DeleteObjectCommand({ Bucket: BUCKET, Key: key });
 
+  const results = [];
+  const readStream = new Readable({
+    read() {
+      s3client
+        .send(getCmd)
+        .then((data) => {
+          const body = data.Body;
+
+          body
+            .on("data", (chunk) => {
+              readStream.push(chunk);
+            })
+            .on("end", () => {
+              readStream.push(null);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  });
   try {
-    const object = await client.send(getObjectCmd);
-    await client.send(copyCmd);
-    await client.send(deleteCmd);
-    console.log(object);
-    await parseCSV(object.Body).each(sendMessage);
+    readStream
+      .pipe(csv())
+      .on("data", (data) => {
+        console.log(data);
+        sqsClient.send(
+          new SendMessageCommand({
+            QueueUrl: SQS_QUEUE_URL,
+            MessageBody: JSON.stringify(data),
+          })
+        );
+      })
+      .on("end", async () => {
+        console.log("end", results);
+        await s3client.send(copyCmd);
+        await s3client.send(deleteCmd);
+      });
 
     console.log("File parsed");
 
