33fbce3b4cb290345661b219ddd66543c9cfd342
George Stoychev <georgi.stoychev.-nd@disney.com>
2023-11-21 13:25:20 +0200

    Changes related to task 8

--- cart-service/src/cart/services/cart.service.ts
+++ cart-service/src/cart/services/cart.service.ts
@@ -1,31 +1,102 @@
 import { Injectable } from '@nestjs/common';
 
-import { v4 } from 'uuid';
-
 import { Cart } from '../models';
+import { getPgClient } from '../../shared';
+
+type CartItemModel = {
+  id: string;
+  count: string;
+  product_id: string;
+};
+
+const findByIdQuery = ({ userId }: { userId: string }) => ({
+  text: `SELECT id, cart_items.product_id, cart_items.count FROM carts FULL JOIN cart_items ON cart_items.cart_id = carts.id where user_id = $1 and status = 'OPEN'`,
+  values: [userId],
+});
+
+const createByQuery = ({ userId }: { userId: string }) => ({
+  text: `INSERT INTO carts (user_id, created_at, updated_at, status) VALUES ($1, now(), now(), 'OPEN')`,
+  values: [userId],
+});
+
+const finishOrderQuery = ({ userId }: { userId: string }) => ({
+  text: `UPDATE carts
+    SET status = 'ORDERED'
+    WHERE user_id = $1
+  `,
+  values: [userId],
+});
+
+const deleteFromCartItems = ({ userId }) => ({
+  text: `DELETE FROM cart_items WHERE cart_id IN (SELECT id FROM carts WHERE user_id = $1 and status = 'OPEN')`,
+  values: [userId],
+});
+
+const insertManyProductsQuery = (
+  items: { count: number; cart_id: string }[],
+) => {
+  const query = {
+    text: `INSERT INTO cart_items (count, cart_id) VALUES ${items
+      .map(({ count, cart_id }) => `(${count}, '${cart_id}')`)
+      .join(',')}`,
+    values: [],
+  };
+
+  return query;
+};
+
+const first = <T>(array: Array<T>) => array[0];
+const mapCartRows = (rows: CartItemModel[]): Cart => {
+  const cartId = first<CartItemModel>(rows).id;
+  return {
+    id: cartId,
+    items: rows
+      .filter(({ product_id }) => Boolean(product_id))
+      .map(({ count, product_id }) => ({
+        count: Number(count),
+        product: {
+          id: product_id,
+          title: '',
+          description: '',
+          price: 0,
+        },
+      })),
+  };
+};
 
 @Injectable()
 export class CartService {
   private userCarts: Record<string, Cart> = {};
 
-  findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+  async findByUserId(userId: string): Promise<Cart> {
+    const client = getPgClient();
+    try {
+      await client.connect();
+      const cartInfo = await client.query(findByIdQuery({ userId }));
+      if (!cartInfo.rows.length) return undefined;
+      return mapCartRows(cartInfo.rows);
+    } catch {
+      return undefined;
+    } finally {
+      await client.end();
+    }
   }
 
-  createByUserId(userId: string) {
-    const id = v4(v4());
-    const userCart = {
-      id,
-      items: [],
-    };
-
-    this.userCarts[ userId ] = userCart;
-
-    return userCart;
+  async createByUserId(userId: string): Promise<Cart> {
+    const client = getPgClient();
+    try {
+      await client.connect();
+      await client.query(createByQuery({ userId }));
+      return await this.findByUserId(userId);
+    } catch {
+      return undefined;
+    } finally {
+      await client.end();
+    }
   }
 
-  findOrCreateByUserId(userId: string): Cart {
-    const userCart = this.findByUserId(userId);
+  async findOrCreateByUserId(userId: string): Promise<Cart> {
+    const userCart = await this.findByUserId(userId);
 
     if (userCart) {
       return userCart;
@@ -34,22 +105,34 @@ export class CartService {
     return this.createByUserId(userId);
   }
 
-  updateByUserId(userId: string, { items }: Cart): Cart {
-    const { id, ...rest } = this.findOrCreateByUserId(userId);
+  async updateByUserId(userId: string, { items }: Cart): Promise<Cart> {
+    const { id } = await this.findOrCreateByUserId(userId);
 
-    const updatedCart = {
-      id,
-      ...rest,
-      items: [ ...items ],
+    const client = getPgClient();
+    try {
+      await client.connect();
+      await client.query(deleteFromCartItems({ userId }));
+      const newItems = items.map(item => ({
+        cart_id: id,
+        count: item.count,
+      }));
+      await client.query(insertManyProductsQuery(newItems));
+      return this.findByUserId(userId);
+    } catch (e) {
+      return undefined;
+    } finally {
+      await client.end();
     }
-
-    this.userCarts[ userId ] = { ...updatedCart };
-
-    return { ...updatedCart };
   }
 
-  removeByUserId(userId): void {
-    this.userCarts[ userId ] = null;
+  async removeByUserId(userId) {
+    const client = getPgClient();
+    try {
+      await client.connect();
+      await client.query(finishOrderQuery({ userId }));
+    } catch {
+    } finally {
+      await client.end();
+    }
   }
-
 }
--- cart-service/src/shared/models-rules/index.ts
+++ cart-service/src/shared/models-rules/index.ts
@@ -1,9 +1,11 @@
 import { AppRequest } from '../models';
 
+const mockedUserId = 'f6dbd6cc-3238-40cc-994b-8938c105a108';
+
 /**
  * @param {AppRequest} request
  * @returns {string}
  */
 export function getUserIdFromRequest(request: AppRequest): string {
-  return request.user && request.user.id;
+  return mockedUserId || (request.user && request.user.id);
 }
